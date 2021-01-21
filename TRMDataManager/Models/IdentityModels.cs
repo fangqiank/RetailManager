42f6adfa3e571f6a13099d851a3debe36f7b16fb
fangqiank <fangqiank@gmail.com>
2021-01-21 09:13:10 -0700

    Update EF Database Name. Database name to be EFData

--- TRMDataManager/Models/IdentityModels.cs
+++ TRMDataManager/Models/IdentityModels.cs
@@ -1,8 +1,7 @@
-﻿using System.Security.Claims;
-using System.Threading.Tasks;
-using Microsoft.AspNet.Identity;
+﻿using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
-using Microsoft.AspNet.Identity.Owin;
+using System.Security.Claims;
+using System.Threading.Tasks;
 
 namespace TRMDataManager.Models
 {
@@ -21,7 +20,7 @@ namespace TRMDataManager.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("EFData", throwIfV1Schema: false)
         {
         }
         
